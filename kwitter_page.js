33c1d0b45937a17c934cb60ef7755efb642286df
xDave12x <77081815+xDave12x@users.noreply.github.com>
2021-06-17 18:36:37 -0400

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -33,8 +33,24 @@ function getData() { firebase.database().ref("/"+room_name).on('value', function
          firebase_message_id = childKey;
          message_data = childData;
 //Start code
-
+name=message_data['name'];
+message=message_data['message'];
+like=message_data['like'];
+name_tag="<h4>"+name+"<img class='user_tick' src='tick.png'></h4>";
+message_tag="<h4 class='message_h4'>"+message+"</h4>";
+like_button ="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='updateLike(this.id)'>";
+span_tag="<span class='glyphicon glyphicon-thumbs-up'>like:"+like+"</span></button> <hr>";
+row=name_tag+message_tag+like_button+span_tag;
+document.getElementById("output").innerHTML+=row;
 //End code
       } });  }); }
 getData();
 
+function updateLike(message_id){
+button_id=message_id;
+like=document.getElementById(button_id).value;
+update_like=Number(like)+1;
+firebase.database().ref(room_name).child(message_id).update({
+      like:update_like
+});
+}
